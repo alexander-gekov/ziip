f0cb8d7a8e6f2e6753369dc89594ff0bd56c6649
Aleksandar Gekov <alexander.gekov00@gmail.com>
2025-06-20 22:05:00 +0300

    Update .gitignore, package.json, and nuxt.config.ts for improved configuration and dependencies

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,13 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   typescript: {
     strict: true,
-    typeCheck: true
+    typeCheck: true,
   },
-  modules: [
-    '@pinia/nuxt',
-    '@nuxtjs/tailwindcss'
-  ]
-})
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Ziip | Connect the dots",
+      meta: [
+        {
+          name: "description",
+          content: "Ziip: Open-source clone of LinkedIn's Zip Game",
+        },
+      ],
+    },
+  },
+});
